5ade1710dbad25d6434ad565dccc029fbb877453
José Ricardo David <david_Ricardo@live.com>
2023-05-03 21:07:58 -0300

    Add project files.

---
+++ ConsumerRabbitMQ/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace ConsumerRabbitMQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+            using (var connection = factory.CreateConnection())
+
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "fila_1"
+                    , false,
+                    false,
+                    false,
+                    null);
+
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"[X] Recebida: {message}");
+                };
+                channel.BasicConsume("fila_1", true, consumer);
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
---
+++ PublisherRabbitMQ/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory()
+{
+    HostName = "localhost"
+};
+using (var connection = factory.CreateConnection())
+
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "fila_1"
+        , false,
+        false,
+        false,
+        null);
+
+    string message = "Bem vindo ao RabbitMQ";
+    var body = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("",
+        routingKey: "fila_1",
+         null,
+         body);
+    Console.WriteLine($"[X] Enviada: {message}");
+
+}
+
